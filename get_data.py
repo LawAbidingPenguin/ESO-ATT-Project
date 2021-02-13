622d96e42f7e69122d31b1d969e53e016b9e2309
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-02-13 19:24:52 +0100

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -2,20 +2,30 @@ import re
 import pandas as pd
 import io
 from multiprocessing import Pool
-import time
 
-start = time.perf_counter()
-
-df_list = []
 att_list = [f'ArkadiusTradeToolsSalesData{i:02d}.lua' for i in range(1,17)]
 
 def clean_data(att):
+    # Checking if file contains NA Megaserver data
+    check = None
+    with open(att, 'rb') as file:
+        na_check = file.read()
+        na_check = str(na_check)
+        if 'NA Megaserver' in na_check:
+            check = True
+        else:
+            check = False
+
     with open(att, 'rb') as file:
         dirty_data = file.readlines()
 
+        if check == True:
+            dirty_data = dirty_data[6:-10]
+        elif check == False:
+            dirty_data = dirty_data[6:-4]
+
     dirty_data = [str(x) for x in dirty_data]
     data = [x for x in dirty_data if '[\"unitPrice\"]' not in x]
-    data = data[6:-4]
     data = [data[i:i+11] for i in range(0, len(data), 12)]
 
     for row in data:
@@ -38,10 +48,11 @@ def clean_data(att):
 
     return df
 
-def main():
-    result = Pool().map(clean_data, att_list)
+def att_data():
+    df_list = Pool().map(clean_data, att_list)
+    att_df= pd.concat(df_list)
 
-    att_df= pd.concat(result)
+    return att_df
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     att_data()
