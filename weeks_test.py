188d3da29d4a7f6887866c87097034541e3455c0
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-03-09 00:27:09 +0100

    Add files via upload

--- weeks_test.py
+++ weeks_test.py
@@ -1,8 +1,7 @@
-import os
-import time
 import pandas as pd
-from datetime import datetime
-import get_data
+from csv_data import get_csv
+import io
+from datetime import datetime, timezone
 
 def trade_weeks():
     weeks = pd.interval_range(start=pd.Timestamp('2021-01-01T14', tz='utc'), periods=208, freq='W-TUE')
@@ -11,43 +10,24 @@ def trade_weeks():
 if __name__ == '__main__':
 
     week_intervals = trade_weeks()
-    # starting time when file was modified
-    start_time = os.path.getmtime('ArkadiusTradeToolsSalesData01.lua')
 
     weeks = []
-    for n in range(0, len(week_intervals)):
-        x = n / 52
+    for i in week_intervals.to_tuples():
+        weeks.append((int(i[0].to_pydatetime().timestamp()), int(i[1].to_pydatetime().timestamp())))
 
-        if x >= 1:
-            weeks.append(f"{str(week_intervals[n].left)[:4]}_week{n-(int(x)*52)+1}")   
+    current = datetime.now(timezone.utc)
+    def get_week(interval):
+        start = datetime.fromtimestamp(interval[0], timezone.utc)
+        end = datetime.fromtimestamp(interval[1], timezone.utc)
+        if start <= current <= end:
+            return True
         else:
-            weeks.append(f"{str(week_intervals[n].left)[:4]}_week{n+1}")
+            return False
 
-    att_df = get_data.att_data()
-    att_df['date'] = att_df['timestamp'].apply(lambda x: datetime.utcfromtimestamp(x))
-    att_df['interval'] = pd.cut(att_df['date'], bins=week_intervals)
-    # att_df['week'] = att_df['interval'].apply(lambda x: list(week_intervals).index(x)+1)
-    # att_df.drop('interval', axis=1, inplace=True)
-    att_df = att_df[att_df['guild_name'].isin(['Rolling Coins', 'Shining Coins', 'Flipping Coins'])]
-    [print(weeks[list(week_intervals).index(x)]) for x in att_df.head()['interval'].values]
+    for interval in weeks:
+        if get_week(interval):
+            # Week index
+            wi = weeks.index(interval)
 
-    # print(att_df.head())
-
-
-    # while True:
-    #     time.sleep(1)
-    #     # new time when file was last modified
-    #     new_time = os.path.getmtime('ArkadiusTradeToolsSalesData01.lua')
-
-    #     if start_time != new_time:
-            
-    #         att_df = get_data.att_data()
-    #         att_df['date'] = att_df['timestamp'].apply(lambda x: datetime.utcfromtimestamp(x))
-    #         att_df['interval'] = pd.cut(att_df['date'], bins=week_intervals)
-    #         # att_df['week'] = att_df['interval'].apply(lambda x: list(week_intervals).index(x)+1)
-    #         # att_df.drop('interval', axis=1, inplace=True)
-    #         att_df = att_df[att_df['guild_name'].isin(['Rolling Coins', 'Shining Coins', 'Flipping Coins'])]
-
-    #         start_time = new_time
-
-    
+    df = pd.read_csv(io.StringIO(get_csv()))
+    print(df[ (df['seller_name'] == 'Fiktius') & (df['timestamp'].between(weeks[wi][0], weeks[wi][1]) )]['price'].sum())
