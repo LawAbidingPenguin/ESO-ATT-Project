b01c08ceef207de05300361c1b33d4f723fc599e
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-03-09 00:31:45 +0100

    Add files via upload

--- weeks_test.py
+++ weeks_test.py
@@ -7,22 +7,21 @@ def trade_weeks():
     weeks = pd.interval_range(start=pd.Timestamp('2021-01-01T14', tz='utc'), periods=208, freq='W-TUE')
     return weeks
 
+current = datetime.now(timezone.utc)
+def get_week(interval):
+    start = datetime.fromtimestamp(interval[0], timezone.utc)
+    end = datetime.fromtimestamp(interval[1], timezone.utc)
+    if start <= current <= end:
+        return True
+    else:
+        return False
+
 if __name__ == '__main__':
 
     week_intervals = trade_weeks()
-
     weeks = []
     for i in week_intervals.to_tuples():
         weeks.append((int(i[0].to_pydatetime().timestamp()), int(i[1].to_pydatetime().timestamp())))
-
-    current = datetime.now(timezone.utc)
-    def get_week(interval):
-        start = datetime.fromtimestamp(interval[0], timezone.utc)
-        end = datetime.fromtimestamp(interval[1], timezone.utc)
-        if start <= current <= end:
-            return True
-        else:
-            return False
 
     for interval in weeks:
         if get_week(interval):
