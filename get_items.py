ad235b9651c472270c1fa465e0214ca0c2b2b987
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-03-17 16:39:48 +0100

    Add files via upload

--- get_items.py
+++ get_items.py
@@ -6,8 +6,10 @@ import re
 if __name__ == '__main__':
 
     df = pd.read_csv('test_file.csv', index_col=9)
-    item_id = re.search('(?<=\:)\d+', df.iloc[2]['item_link']).group()
-    print(item_id)
+    sales_per_item = df['item_link'].value_counts()
+
+    item_id = re.search('(?<=\:)\d+', sales_per_item.index[0]).group()
+    # print(item_id)
     # item_id = 54173
 
     session = req.HTMLSession()
@@ -15,5 +17,5 @@ if __name__ == '__main__':
     links = r.html.absolute_links
     img_check = f'itemid={item_id}&summary&none=item.png'
     links = [l for l in links if img_check in l]
-    img = links[0]
-    print(img)
+    most_sold = links[0]
+    print(most_sold)
