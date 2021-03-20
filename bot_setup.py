f3f3234e9dbd8945803fe8210e05593551579d4c
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-03-20 02:13:44 +0100

    Add files via upload

--- bot_setup.py
+++ bot_setup.py
@@ -2,8 +2,9 @@ import discord
 import pandas as pd
 from datetime import datetime, timezone
 
+# creating custom trade week intervals
 week_intervals = pd.interval_range(start=pd.Timestamp('2021-01-01T14', tz='utc'), periods=208, freq='W-TUE')
-# turning week_intervals to timestamp intervals
+# turning week_intervals to timestamp intervals (to be used with get_week()) 
 weeks = []
 for i in week_intervals.to_tuples():
     weeks.append((int(i[0].to_pydatetime().timestamp()), int(i[1].to_pydatetime().timestamp())))
@@ -25,16 +26,35 @@ for interval in weeks:
         wi = weeks.index(interval)
 
 df = pd.read_csv('test_file.csv', index_col=9)
-
+# getting current trade week sales
 def current_sales(seller, interval=[weeks[wi][0], weeks[wi][1]]):
-    sales = df[(df['seller_name'].str.lower() == seller.lower()) & (df['timestamp'].between(interval[0],interval[1]))]['price'].sum()
-    return sales
 
+    if '\'' in seller:
+        seller = '\\\''.join(seller.split('\''))
+
+    name = df[df['seller_name'].str.lower() == seller.lower()]['seller_name'].unique()[0]
+    sales = df[(df['seller_name'].str.lower() == seller.lower()) & (df['timestamp'].between(interval[0],interval[1]))]['price'].sum()
+
+    if '\\' in name:
+        name = name.replace('\\', '')
+
+    return (name, sales)
+
+# getting previous trade week sales
 def previous_sales(seller, interval=[weeks[wi-1][0], weeks[wi-1][1]]):
-    sales = df[(df['seller_name'].str.lower() == seller.lower()) & (df['timestamp'].between(interval[0],interval[1]))]['price'].sum()
-    return sales
 
-# adding spaces to sales 
+    if '\'' in seller:
+        seller = '\\\''.join(seller.split('\''))
+
+    name = df[df['seller_name'].str.lower() == seller.lower()]['seller_name'].unique()[0]
+    sales = df[(df['seller_name'].str.lower() == seller.lower()) & (df['timestamp'].between(interval[0],interval[1]))]['price'].sum()
+
+    if '\\' in name:
+        name = name.replace('\\', '')
+
+    return (name, sales)
+
+# adding spaces to sales number for readability
 def add_spaces(num):
     num = str(num)
     new_num = list(num)
@@ -44,7 +64,6 @@ def add_spaces(num):
     else:
         for i in range(len(num)%3, len(num)+int(len(num)/3), 4):
             new_num.insert(i, ' ')
-        
 
     return ''.join(new_num)
 
@@ -59,10 +78,14 @@ class MyClient(discord.Client):
 
         if msg.startswith('!sales'):
             seller = msg.split()[1]
-            await chl.send(f"`{seller}: {add_spaces(current_sales(seller))}`")
-        elif msg.startswith('!last'):
+            data = current_sales(seller)
+
+            await chl.send(f"`{data[0]}: {add_spaces(data[1])}`")
+        elif msg.startswith('!lastweek'):
             seller = msg.split()[1]
-            await chl.send(f"`{seller}: {add_spaces(previous_sales(seller))}`")
+            data = previous_sales(seller)
+
+            await chl.send(f"`{data[0]}: {add_spaces(data[1])}`")
 
 if __name__ == '__main__':
 
