4cc7c775be8e9eb7a7cb42301cd6e4f9597156e9
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-03-28 23:47:44 +0200

    Add files via upload

--- update_csv.py
+++ update_csv.py
@@ -1,7 +1,68 @@
-from csv_data import get_csv
+import re
+from os.path import getmtime, isfile
+from multiprocessing import Pool
 
+guilds = ['Rolling Coins', 'Flipping Coins', 'Shining Coins']
 
-if __name__ == '__main__':
+att_list = [f'ArkadiusTradeToolsSalesData{i:02d}.lua' for i in range(1,17)]
+columns = 'buyer_name,guild_name,internal,item_link,price,quantity,seller_name,taxes,timestamp,entry'
 
-    with open('test_file.csv', 'w') as f:
-        f.write(get_csv())
+def clean_data(att):
+    # Checking if file contains NA Megaserver data
+    check = None
+    with open(att, 'rb') as file:
+        na_check = file.read()
+        na_check = str(na_check)
+        if 'NA Megaserver' in na_check:
+            check = True
+        else:
+            check = False
+
+    with open(att, 'rb') as file:
+        dirty_data = file.readlines()
+
+        if check == True:
+            if "NA Megaserver" in na_check[:70]:
+                dirty_data = dirty_data[12:-4]
+            else:
+                dirty_data = dirty_data[6:-10]
+
+        elif check == False:
+            dirty_data = dirty_data[6:-4]
+
+    dirty_data = [str(x) for x in dirty_data]
+    data = [x for x in dirty_data if '[\"unitPrice\"]' not in x]
+    data = [data[i:i+11] for i in range(0, len(data), 12)]
+
+    for row in data:
+        row.pop(1)
+        row.sort()
+        for x in range(0, len(row)-1):
+            row[x] = row[x].split('=')[1].split(',')[0].strip()
+            row[x] = row[x].strip('\"')
+
+        row[9] = re.sub('\[|\]', '', re.search('\d+',row[9]).group())
+
+    data = [x for x in data if x[1] in guilds]
+    data = [','.join(row) for row in data]
+    data = [x.replace('@', '') for x in data]
+    data = '\n'.join(data)
+
+    return data
+    
+def get_csv():
+    csv_list = Pool().map(clean_data, att_list)
+    csv = columns + '\n' + '\n'.join(csv_list)
+
+    return csv
+
+# if __name__ == '__main__':
+    
+    # if not isfile('att_data.csv'):
+    #     with open('att_data.csv', 'w') as f:
+    #         f.write(get_csv())        
+
+    # mod_time = getmtime('att_data.csv')
+
+    # with open('att_data.csv', 'w') as f:
+    #     f.write(get_csv())
