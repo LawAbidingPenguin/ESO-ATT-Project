81286dfd556f2480aa794598b4eb43f518a7221e
Uros Mladenovic <70856475+LawAbidingPenguin@users.noreply.github.com>
2021-04-07 01:37:17 +0200

    Add files via upload

--- bot_test.py
+++ bot_test.py
@@ -66,10 +66,10 @@ def add_spaces(num):
     new_num = list(num)
     if len(num) % 3 == 0:
         for i in range(3, len(num)+int(len(num)/3)-1, 4):
-            new_num.insert(i, ' ')
+            new_num.insert(i, ',')
     else:
         for i in range(len(num)%3, len(num)+int(len(num)/3), 4):
-            new_num.insert(i, ' ')
+            new_num.insert(i, ',')
 
     return ''.join(new_num)
 
@@ -80,9 +80,8 @@ intents.members = True
 bot = Bot(command_prefix='!', intents=intents)
 
 @bot.command()
-async def thisweek(cxt):
-    msg = cxt.message.content
-    chl = cxt.channel
+async def thisweek(ctx):
+    msg = ctx.message.content
 
     # week index
     wi = get_week()
@@ -101,19 +100,17 @@ async def thisweek(cxt):
     bot_img = bot.user.avatar_url
 
     embed = discord.Embed(title=f"{seller} current trade week sales", colour=0xFFD700)
-    embed.add_field(name='Rolling Coins', value=f"{add_spaces(guild_sales['Rolling Coins'])}", inline=False)
-    embed.add_field(name='Shining Coins', value=f"{add_spaces(guild_sales['Shining Coins'])}", inline=False)
-    embed.add_field(name='Flipping Coins', value=f"{add_spaces(guild_sales['Flipping Coins'])}", inline=False)
+    for g in guilds:
+        embed.add_field(name=f'{g}', value=f"{add_spaces(guild_sales[g])}", inline=False)
     embed.add_field(name='All Sales', value=f"{add_spaces(sales)}", inline=False)
     embed.set_footer(text=f"Last updated(UTC): {file_mod_time}")
     embed.set_thumbnail(url=bot_img)
 
-    await chl.send(embed=embed)
+    await ctx.send(embed=embed)
 
 @bot.command()
-async def lastweek(cxt):
-    msg = cxt.message.content
-    chl = cxt.channel 
+async def lastweek(ctx):
+    msg = ctx.message.content 
 
     wi = get_week()
 
@@ -131,20 +128,18 @@ async def lastweek(cxt):
     bot_img = bot.user.avatar_url
 
     embed = discord.Embed(title=f"{seller} previous trade week sales", colour=0xFFD700)
-    embed.add_field(name='Rolling Coins', value=f"{add_spaces(guild_sales['Rolling Coins'])}", inline=False)
-    embed.add_field(name='Shining Coins', value=f"{add_spaces(guild_sales['Shining Coins'])}", inline=False)
-    embed.add_field(name='Flipping Coins', value=f"{add_spaces(guild_sales['Flipping Coins'])}", inline=False)
+    for g in guilds:
+        embed.add_field(name=f'{g}', value=f"{add_spaces(guild_sales[g])}", inline=False)
     embed.add_field(name='All Sales', value=f"{add_spaces(sales)}", inline=False)
     embed.set_footer(text=f"Last updated(UTC): {file_mod_time}")
     embed.set_thumbnail(url=bot_img)
 
-    await chl.send(embed=embed)
+    await ctx.send(embed=embed)
 
 # get sales for n previous days
 @bot.command()
-async def days(cxt):
-    msg = cxt.message.content.replace('!days ', '')
-    chl = cxt.channel 
+async def days(ctx):
+    msg = ctx.message.content.replace('!days ', '') 
 
     # number of days
     d = int(msg.split()[0])
@@ -163,22 +158,30 @@ async def days(cxt):
     bot_img = bot.user.avatar_url
 
     embed = discord.Embed(title=f"{seller} last {d} days sales", colour=0xFFD700)
-    embed.add_field(name='Rolling Coins', value=f"{add_spaces(guild_sales['Rolling Coins'])}", inline=False)
-    embed.add_field(name='Shining Coins', value=f"{add_spaces(guild_sales['Shining Coins'])}", inline=False)
-    embed.add_field(name='Flipping Coins', value=f"{add_spaces(guild_sales['Flipping Coins'])}", inline=False)
+    for g in guilds:
+        embed.add_field(name=f'{g}', value=f"{add_spaces(guild_sales[g])}", inline=False)
     embed.add_field(name='All Sales', value=f"{add_spaces(sales)}", inline=False)
     embed.set_footer(text=f"Last updated(UTC): {file_mod_time}")
     embed.set_thumbnail(url=bot_img)
 
-    await chl.send(embed=embed)
+    await ctx.send(embed=embed)
 
 # get most sold item
 @bot.command()
-async def mostsold(cxt):
-    chl = cxt.channel
+async def mostsold(ctx):
 
-    most_sold = df['item_link'].value_counts().idxmax()
-    item_id = re.search('(?<=\:)\d+', most_sold).group()
+    wi = get_week()
+    interval = [ weeks[wi][0], weeks[wi][1]]
+
+    cw_df = df[df['timestamp'].between(interval[0], interval[1])]
+
+    most_sold = cw_df['item_link'].value_counts()
+    ms_link = most_sold.idxmax()
+    ms_sales = most_sold.iloc[0]
+    avg_price_cw = int(cw_df[cw_df['item_link']==ms_link]['price'].mean())
+    avg_price_30 = int(df[df['item_link']==ms_link]['price'].mean())
+
+    item_id = re.search('(?<=\:)\d+', ms_link).group()
 
     session = req.HTMLSession()
     r = session.get(f'https://esoitem.uesp.net/itemSearch.php?version=&text={item_id}')
@@ -189,15 +192,28 @@ async def mostsold(cxt):
     # most sold image
     ms_img = links[0]
 
-    await chl.send(ms_img)
+    file_mod_time = datetime.fromtimestamp(os.path.getmtime('att_data.csv'), 
+                    timezone.utc).strftime("%H:%M:%S %d-%m-%Y ")
+
+    embed = discord.Embed(title='Most sold', description='Most sold item during current trade week', 
+                                             colour=0xFFD700)
+    embed.add_field(name='Trades made', value=f'{ms_sales}', inline=False)
+    embed.add_field(name='Average price this trade week', value=f'{add_spaces(avg_price_cw)}', inline=False)
+    embed.add_field(name='Average price last 30 days', value=f'{add_spaces(avg_price_30)}', inline=False)
+    embed.set_footer(text=f"Last updated(UTC): {file_mod_time}")
+    embed.set_image(url=ms_img)
+
+    await ctx.send(embed=embed)
 
 # get most valuable item
 @bot.command()
-async def mostvaluable(cxt):
-    chl = cxt.channel
+async def mostvaluable(ctx):
 
-    mv_row = df.loc[df['price'].idxmax()]
-    
+    wi = get_week()
+    interval = [ weeks[wi][0], weeks[wi][1]]
+    cw_df = df[df['timestamp'].between(interval[0], interval[1])]
+
+    mv_row = cw_df.loc[cw_df['price'].idxmax()]
     item_id = re.search('(?<=\:)\d+', mv_row['item_link']).group()
 
     session = req.HTMLSession()
@@ -210,14 +226,18 @@ async def mostvaluable(cxt):
     file_mod_time = datetime.fromtimestamp(os.path.getmtime('att_data.csv'), 
                     timezone.utc).strftime("%H:%M:%S %d-%m-%Y ")
 
-    await chl.send(f"{mv_img}"
-                   f"\n```Sold for {add_spaces(mv_row['price'])} by {mv_row['seller_name']}"
-                   f"\nLast updated(UTC): {file_mod_time}```")
+    embed = discord.Embed(title='Most valuable', description='Highest trade made during current trade week', 
+                                             colour=0xFFD700)
+    embed.add_field(name='Seller', value=f"{mv_row['seller_name']}", inline=False)
+    embed.add_field(name='Price', value=f"{add_spaces(mv_row['price'])}", inline=False)
+    embed.set_footer(text=f"Last updated(UTC): {file_mod_time}")
+    embed.set_image(url=mv_img)
+
+    await ctx.send(embed=embed)
 
 @bot.command()
-async def top5(cxt):
-    chl = cxt.channel
-    msg = cxt.message.content
+async def top5(ctx):
+    msg = ctx.message.content
     nl = '\n' # newline to be used in f-string
     ws = '\u200b' # whitespace
     wi = get_week() # week index
@@ -234,9 +254,7 @@ async def top5(cxt):
             t5.sort_values(ascending=False, inplace=True)
 
             t5_pairs = [f"{t5.index[n]}: {ws} {ws} {ws} {ws} {add_spaces(t5.iloc[n])}" for n in range(5)]
-
-            embed.add_field(name=f'{g}', value=f"{nl.join(t5_pairs)}",
-                                               inline=False)
+            embed.add_field(name=f'{g}', value=f"{nl.join(t5_pairs)}", inline=False)
 
         # all guild sales
         ags = df[df['timestamp'].between(i[0], i[1])].groupby('seller_name')['price'].sum()
@@ -257,9 +275,7 @@ async def top5(cxt):
             t5.sort_values(ascending=False, inplace=True)
 
             t5_pairs = [f"{t5.index[n]}: {ws} {ws} {ws} {ws} {add_spaces(t5.iloc[n])}" for n in range(5)]
-            
-            embed.add_field(name=f'{g}', value=f"{nl.join(t5_pairs)}",
-                                               inline=False)
+            embed.add_field(name=f'{g}', value=f"{nl.join(t5_pairs)}", inline=False)
 
         # all guild buys
         agb = df[(df['timestamp'].between(i[0], i[1])) &
@@ -267,23 +283,23 @@ async def top5(cxt):
         agb.sort_values(ascending=False, inplace=True)
 
         agb_pairs = [f'{agb.index[n]}: {ws} {ws} {ws} {ws} {add_spaces(agb.iloc[n])}' for n in range(5)]
-
-        embed.add_field(name='All Guilds', value=f"{nl.join(agb_pairs)}",
-                                                 inline=False)
+        embed.add_field(name='All Guilds', value=f"{nl.join(agb_pairs)}", inline=False)
     else:
         embed = discord.Embed(title=f'No option **{msg.split()[1]}**, please choose **traders** or **buyers**')
 
-    await chl.send(embed=embed)
+    await ctx.send(embed=embed)
 
 @bot.command()
-async def summary(cxt):
-    chl = cxt.channel
+async def summary(ctx):
+    #TODO add total sales for each guild
+
     nl = '\n' # newline to be used in f-string
     wi = get_week()
     i = [ weeks[wi-1][0], weeks[wi-1][1]] # interval
     ws = '\u200b' # whitespace
+    cw = datetime.now(timezone.utc).isocalendar()[1] # current week to be displayed in the title
 
-    embed = discord.Embed(title=f"Trading summary: Week {wi+1}", colour=0xFFD700)
+    embed = discord.Embed(title=f"Trading summary: Week {cw}", colour=0xFFD700)
     
     for g in guilds:
         # ts - top sellers
@@ -327,15 +343,26 @@ async def summary(cxt):
     embed.add_field(name=f'{ws}\n', value="Internal purchases;\n\n"
                                           f"{nl.join(agb_pairs)}")
     embed.add_field(name=f'{ws}', value=f'{ws}')
-    
-    await chl.send(embed=embed)
+
+    total_sales = df[df['timestamp'].between(i[0], i[1])]['price'].sum()
+    total_buys = df[(df['timestamp'].between(i[0], i[1])) &
+                 (df['internal']==1)]['price'].sum()
+    embed.add_field(name='Total', value="Sales;\n\n"
+                                        f"{add_spaces(total_sales)}\n\n")
+    embed.add_field(name=f'{ws}\n', value='Purchases;\n\n'
+                                          f'{add_spaces(total_buys)}\n\n')
+
+    file_mod_time = datetime.fromtimestamp(os.path.getmtime('att_data.csv'), 
+                    timezone.utc).strftime("%H:%M:%S %d-%m-%Y ")
+    embed.set_footer(text=f"Last updated(UTC): {file_mod_time}")
+
+    await ctx.send(embed=embed)
 
 # most frequent buyers
 @bot.command()
-async def admirer(cxt):
+async def admirer(ctx):
 
-    msg = cxt.message.content
-    chl = cxt.channel
+    msg = ctx.message.content
     
     msg_input = msg.split()[1]
     seller = fix_seller_name(msg_input)[1]
@@ -344,13 +371,12 @@ async def admirer(cxt):
     buyer_name = buyer_row.idxmax()
     purchases = buyer_row.iloc[0]
 
-    await chl.send(f"`Most frequent buyer(30 days) is {buyer_name} with {purchases} purchases.`")
+    await ctx.send(f"`Most frequent buyer(30 days) is {buyer_name} with {purchases} purchases.`")
 
 @bot.command()
-async def guildadmirer(cxt):
+async def guildadmirer(ctx):
 
-    msg = cxt.message.content
-    chl = cxt.channel
+    msg = ctx.message.content
     
     msg_input = msg.split()[1]
     seller = fix_seller_name(msg_input)[1]
@@ -359,15 +385,17 @@ async def guildadmirer(cxt):
     buyer_name = buyer_row.idxmax()
     purchases = buyer_row.iloc[0]
 
-    await chl.send(f"`Most frequent guildie buyer(30 days) is {buyer_name} with {purchases} purchases.`")
+    await ctx.send(f"`Most frequent guildie buyer(30 days) is {buyer_name} with {purchases} purchases.`")
+
 
 @bot.event
-async def on_command_error(cxt, error):
-    msg_input = cxt.message.content.split()[1]
+async def on_command_error(ctx, error):
 
     if isinstance(error, discord.ext.commands.CommandInvokeError):
-        await cxt.send(f'`No info found for {msg_input}...`')
+        print(error)
+        await ctx.send(f'`No info found...`')
     #TODO add code to write to an error logging file
+
 
 if __name__ == '__main__':
 
